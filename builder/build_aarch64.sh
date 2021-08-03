ce93b61c8d0a78334f73516250a8facc887c7b85
Luca Carlon <carlon.luca@gmail.com>
2021-08-03 13:51:40 +0200

    Added dockerfile for the builder.

---
+++ builder/build_aarch64.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget --no-check-certificate -q http://download.qt.io/official_releases/qt/5.15/5.15.2/single/qt-everywhere-src-$1.tar.xz
+tar xvfp qt-everywhere-src-$1.tar.xz
+mkdir build
+cd build
+export PKG_CONFIG_PATH=/usr/local/lib/aarch64-linux-gnu/pkgconfig:/usr/local/lib/pkgconfig:/usr/local/share/pkgconfig:/usr/lib/aarch64-linux-gnu/pkgconfig:/usr/lib/pkgconfig:/usr/share/pkgconfig
+../qt-everywhere-src-5.15.2/configure -opensource -confirm-license -release -skip webengine -nomake tests -nomake examples -qt-zlib -qt-libjpeg -qt-libpng -xcb -qt-freetype -qt-pcre -qt-harfbuzz -prefix /opt/Qt-aaarch64-$1 -xplatform linux-aarch64-gnu-g++ -v -sysroot / -pkg-config
+make -j $(($(nproc)+4))
+make install
+cd /opt
+tar cvfpJ /root/export/Qt-aaarch64-$1.tar.xz Qt-aaarch64-$1
