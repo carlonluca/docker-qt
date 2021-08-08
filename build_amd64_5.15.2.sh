c4845eea4fb0bc17055715f1550fde9ee5b5436f
Luca Carlon <carlon.luca@gmail.com>
2021-08-08 18:08:49 +0200

    Update scripts to build 5.15.2.

--- build_amd64_5.15.2.sh
+++ build_amd64_5.15.2.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run --rm --name qt-builder -v $PWD/qt_export:/root/export carlonluca/qt-builder:focal /root/build_amd64.sh 5.15.2
+docker run --rm -it --name qt-builder -v $PWD/qt_export:/root/export \
+    -v $(pwd)/builder/build_amd64.sh:/build_amd64.sh carlonluca/qt-builder:focal \
+    /build_amd64.sh 5.15.2 https://download.qt.io/official_releases/qt/5.15/5.15.2/single/
