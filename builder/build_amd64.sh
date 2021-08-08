0abeada25792c0939475eebef763dfceb940e2fd
Luca Carlon <carlon.luca@gmail.com>
2021-08-08 18:02:54 +0200

    Adapt scripts to build packages on other urls.

--- builder/build_amd64.sh
+++ builder/build_amd64.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-wget --no-check-certificate http://download.qt.io/official_releases/qt/5.15/5.15.2/single/qt-everywhere-src-$1.tar.xz
+wget --no-check-certificate "$2/qt-everywhere-src-$1.tar.xz"
 tar xvfp qt-everywhere-src-$1.tar.xz
 mkdir build
 cd build
-../qt-everywhere-src-5.15.2/configure -opensource -confirm-license -release -nomake tests -nomake examples -qt-zlib -qt-libjpeg -qt-libpng -xcb -qt-freetype -qt-pcre -qt-harfbuzz -prefix /opt/Qt-amd64-$1 -v -pkg-config
+../qt-everywhere-src-$1/configure -opensource -confirm-license -release -nomake tests -nomake examples -qt-zlib -qt-libjpeg -qt-libpng -xcb -qt-freetype -qt-pcre -qt-harfbuzz -prefix /opt/Qt-amd64-$1 -v -pkg-config
 make -j $(($(nproc)+4))
 make install
 cd /opt
