8ef2a0f15257c18bca242c9a99cc7dec9ac269d3
Luca Carlon <carlon.luca@gmail.com>
2021-10-02 10:27:31 +0200

    Update tag to build older qt versions.

--- build_6.1.3.sh
+++ build_6.1.3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker run --rm -it --name qt-builder -v $PWD/qt_export:/root/export \
-    -v $(pwd)/builder/build_qt6.sh:/build_qt6.sh carlonluca/qt-builder:focal \
+    -v $(pwd)/builder/build_qt6.sh:/build_qt6.sh carlonluca/qt-builder:1 \
     /build_qt6.sh 6.1.3 https://download.qt.io/official_releases/qt/6.1/6.1.3/single/
