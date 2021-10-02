ecc243dc1bb71a29036b2592473bf817abafd50d
Luca Carlon <carlon.luca@gmail.com>
2021-10-02 10:25:32 +0200

    Temporarily disable qtwebengine.

--- builder/build_qt6.sh
+++ builder/build_qt6.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 wget --no-check-certificate "$2/qt-everywhere-src-$1.tar.xz"
 tar xvfp qt-everywhere-src-$1.tar.xz
 mkdir build
@@ -8,7 +10,7 @@ cd build
 cmake --build . --parallel $(($(nproc)+4))
 cmake --install .
 rm -rf *
-../qt-everywhere-src-$1/configure -release -nomake examples -nomake tests -qt-host-path /opt/Qt-amd64-$1 -prefix /opt/Qt-arm64-$1 -- -DCMAKE_TOOLCHAIN_FILE=/root/toolchain.cmake -DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON -DCMAKE_BUILD_WITH_INSTALL_RPATH=/opt/Qt-arm64-$1
+../qt-everywhere-src-$1/configure -release -nomake examples -nomake tests -skip qtwebengine -qt-host-path /opt/Qt-amd64-$1 -prefix /opt/Qt-arm64-$1 -- -DCMAKE_TOOLCHAIN_FILE=/root/toolchain.cmake -DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON -DCMAKE_BUILD_WITH_INSTALL_RPATH=/opt/Qt-arm64-$1
 cmake --build . --parallel $(($(nproc)+4))
 cmake --install .
 
