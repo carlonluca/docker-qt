94a356293caf31218b4b21acb9c152e14310637e
Luca Carlon <carlon.luca@gmail.com>
2022-04-07 23:43:20 +0200

    Build Qt 6.2.4 with the newer builder image.

--- build_6.2.4.sh
+++ build_6.2.4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker run --rm -it --name qt-builder -v $PWD/qt_export:/root/export \
-    -v $(pwd)/builder/build_qt6.sh:/build_qt6.sh carlonluca/qt-builder:2 \
+    -v $(pwd)/builder/build_qt6.sh:/build_qt6.sh carlonluca/qt-builder:3 \
     /build_qt6.sh 6.2.4 https://download.qt.io/official_releases/qt/6.2/6.2.4/single/
