fcd04b9409306eb75943d4ddd397315c45c6fb55
Luca Carlon <carlon.luca@gmail.com>
2022-06-24 23:53:23 +0200

    Do not build webengine for arm64.

--- builder/build_qt6.sh
+++ builder/build_qt6.sh
@@ -10,7 +10,7 @@ cd build
 cmake --build . --parallel $(($(nproc)+4))
 cmake --install .
 rm -rf *
-../qt-everywhere-src-$1/configure -release -nomake examples -nomake tests -qt-host-path /opt/Qt-amd64-$1 -prefix /opt/Qt-arm64-$1 -- -DCMAKE_TOOLCHAIN_FILE=/root/toolchain.cmake -DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON -DCMAKE_BUILD_WITH_INSTALL_RPATH=/opt/Qt-arm64-$1
+../qt-everywhere-src-$1/configure -release -nomake examples -nomake tests -skip qtwebengine -qt-host-path /opt/Qt-amd64-$1 -prefix /opt/Qt-arm64-$1 -- -DCMAKE_TOOLCHAIN_FILE=/root/toolchain.cmake -DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON -DCMAKE_BUILD_WITH_INSTALL_RPATH=/opt/Qt-arm64-$1
 cmake --build . --parallel $(($(nproc)+4))
 cmake --install .
 
