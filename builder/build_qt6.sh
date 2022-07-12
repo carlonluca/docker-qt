e02a4f5c9dab5e7a5805e2e754035d4b94543229
homdx <37062532+homdx@users.noreply.github.com>
2022-07-12 22:14:06 +0300

    Added Stored configure options to archieve

--- builder/build_qt6.sh
+++ builder/build_qt6.sh
@@ -7,10 +7,12 @@ tar xvfp qt-everywhere-src-$1.tar.xz
 mkdir build
 cd build
 ../qt-everywhere-src-$1/configure -release -nomake examples -nomake tests -prefix /opt/Qt-amd64-$1
+cp -v config.summary /root/export/Qt-amd64-$1-config.summary
 cmake --build . --parallel $(($(nproc)+4))
 cmake --install .
 rm -rf *
-../qt-everywhere-src-$1/configure -release -nomake examples -nomake tests -skip qtwebengine -qt-host-path /opt/Qt-amd64-$1 -prefix /opt/Qt-arm64-$1 -- -DCMAKE_TOOLCHAIN_FILE=/root/toolchain.cmake -DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON -DCMAKE_BUILD_WITH_INSTALL_RPATH=/opt/Qt-arm64-$1
+../qt-everywhere-src-$1/configure -release -nomake examples -nomake tests -qt-host-path /opt/Qt-amd64-$1 -prefix /opt/Qt-arm64-$1 -- -DCMAKE_TOOLCHAIN_FILE=/root/toolchain.cmake -DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON -DCMAKE_BUILD_WITH_INSTALL_RPATH=/opt/Qt-arm64-$1
+cp -v config.summary /root/export/Qt-arm64-$1-config.summary
 cmake --build . --parallel $(($(nproc)+4))
 cmake --install .
 
