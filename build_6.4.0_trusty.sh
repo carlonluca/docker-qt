13c733c4c1ca84b38665dc3d07baa0d88c955fbb
Luca Carlon <carlon.luca@gmail.com>
2022-10-26 19:08:49 +0200

    Draft of dockerfiles for trusty and bionic.

---
+++ build_6.4.0_trusty.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker run -it --rm --name qt-builder -v $PWD/qt_export:/root/export \
+    -v $(pwd)/builder/build_qt6.sh:/build_qt6.sh carlonluca/qt-builder:trusty \
+    /build_qt6.sh 6.4.0 https://download.qt.io/official_releases/qt/6.4/6.4.0/single/
