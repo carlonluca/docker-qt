e62a8c6ae0041a383c5eacfc1d7110eedcb23feb
Luca Carlon <carlon.luca@gmail.com>
2022-10-31 15:12:34 +0100

    Store qt config summary in the build.

--- builder/build_qt6.sh
+++ builder/build_qt6.sh
@@ -9,11 +9,12 @@ cd build
 ../qt-everywhere-src-$1/configure -release -nomake examples -nomake tests -webengine-proprietary-codecs -prefix /opt/Qt-amd64-$1 -- -DFEATURE_ffmpeg=OFF
 cmake --build . --parallel $(($(nproc)+4))
 cmake --install .
+cp config.summary /opt/Qt-amd64-$1/
 rm -rf *
 ../qt-everywhere-src-$1/configure -release -nomake examples -nomake tests -skip qtwebengine -qt-host-path /opt/Qt-amd64-$1 -prefix /opt/Qt-arm64-$1 -- -DCMAKE_TOOLCHAIN_FILE=/root/toolchain.cmake -DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON -DCMAKE_BUILD_WITH_INSTALL_RPATH=/opt/Qt-arm64-$1  -DFEATURE_ffmpeg=OFF
 cmake --build . --parallel $(($(nproc)+4))
 cmake --install .
-
+cp config.summary /opt/Qt-arm64-$1/
 cd /opt
 tar cvfpJ /root/export/Qt-amd64-$1.tar.xz Qt-amd64-$1
 tar cvfpJ /root/export/Qt-arm64-$1.tar.xz Qt-arm64-$1
