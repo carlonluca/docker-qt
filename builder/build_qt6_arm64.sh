fe4593d258e050c9a9386c034c9924d397bfc18b
Luca Carlon <carlon.luca@gmail.com>
2022-12-26 23:22:35 +0100

    Build Qt without zstd support to be able to build for android.

--- builder/build_qt6_arm64.sh
+++ builder/build_qt6_arm64.sh
@@ -11,7 +11,7 @@ tar xvfp qt-everywhere-src-$1.tar.xz
 mkdir build
 
 cd build
-../qt-everywhere-src-$1/configure -release -nomake examples -nomake tests -skip qtwebengine -qt-host-path /opt/Qt-amd64-$1 -prefix /opt/Qt-arm64-$1 -- -DCMAKE_TOOLCHAIN_FILE=/root/toolchain.cmake -DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON -DCMAKE_BUILD_WITH_INSTALL_RPATH=/opt/Qt-arm64-$1  -DFEATURE_ffmpeg=OFF
+../qt-everywhere-src-$1/configure -no-zstd -release -nomake examples -nomake tests -skip qtwebengine -qt-host-path /opt/Qt-amd64-$1 -prefix /opt/Qt-arm64-$1 -- -DCMAKE_TOOLCHAIN_FILE=/root/toolchain.cmake -DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON -DCMAKE_BUILD_WITH_INSTALL_RPATH=/opt/Qt-arm64-$1  -DFEATURE_ffmpeg=OFF
 cmake --build . --parallel $(($(nproc)+4))
 cmake --install .
 cp config.summary /opt/Qt-arm64-$1/
