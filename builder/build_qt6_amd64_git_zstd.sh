d4e4772a913ae1f2e1eef60688ab923cf5ec0bb7
Luca Carlon <carlon.luca@gmail.com>
2024-04-02 19:57:05 +0200

    Fix build scripts.

--- builder/build_qt6_amd64_git_zstd.sh
+++ builder/build_qt6_amd64_git_zstd.sh
@@ -10,11 +10,11 @@ cd ..
 mkdir build
 
 cd build
-../qt5/configure -release -nomake examples -nomake tests -zstd -webengine-proprietary-codecs -prefix /opt/qt/$1
+../qt5/configure -release -nomake examples -nomake tests -zstd -webengine-proprietary-codecs -prefix /opt/qt/$1/gcc_64
 cmake --build . --parallel $(($(nproc)+4))
 cmake --install .
-cp config.summary /opt/qt/$1
+cp config.summary /opt/qt/$1/gcc_64
 
 cd /opt
-ln -s /opt/qt/$1 /opt/Qt-amd64-$1
+ln -s /opt/qt/$1/gcc_64 /opt/Qt-amd64-$1
 tar cvfpJ /root/export/Qt-amd64-$1.tar.xz Qt-amd64-$1 qt
