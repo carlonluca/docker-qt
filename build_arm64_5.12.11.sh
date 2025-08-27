2a4454d3a9dee2d1824bd38f525f91c912ae209c
RivTian <kannajiahe@gmail.com>
2025-08-27 13:58:51 +0800

    Fix scripts name and content mismatch

--- build_arm64_5.12.11.sh
+++ build_arm64_5.12.11.sh
@@ -2,4 +2,4 @@
 
 docker run --rm -it --name qt-builder -v $PWD/qt_export:/root/export \
     -v $(pwd)/builder/build_aarch64.sh:/build_aarch64.sh carlonluca/qt-builder:focal \
-    /build_aarch64.sh 5.15.2 https://download.qt.io/official_releases/qt/5.15/5.15.2/single/
+    /build_aarch64.sh 5.12.11 https://download.qt.io/official_releases/qt/5.12/5.12.11/single/
