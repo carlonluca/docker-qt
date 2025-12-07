79ffcc21b78080359e109d64eb2a943832a93c98
Luca Carlon <carlon.luca@gmail.com>
2025-12-07 18:17:26 +0100

    Added dockerfiles for qt 6.10.1.

---
+++ 6.10.1/build_6.10.1_amd64.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+#
+# Author:  Luca Carlon
+# Company: -
+# Date:    2025.11.22
+#
+
+qt_version=6.10.1
+qt_src=qt-src
+
+podman run -it --rm --name qt-builder \
+    -v "$PWD/../qt_export":/root/export \
+    -v "$qt_src":/qt \
+    -v "$(pwd)/../builder/build_qt6_amd64_git_zstd_2.sh:/build_qt6_amd64.sh" \
+    docker.io/carlonluca/qt-builder:noble-17-36-27.2.12479018 bash -c '
+
+set -e
+
+apt-get update
+apt-get install -y ca-certificates curl gnupg
+
+# WebEngine
+apt-get install -y llvm-dev libclang-dev clang
+apt-get install -y python3-venv python3-full
+python3 -m venv ~/qt-build-env
+source ~/qt-build-env/bin/activate
+pip3 install spdx-tools
+pip3 install html5lib
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=24
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+
+# BUILD
+cd /qt
+if [ ! -d /qt/qt5 ]; then
+    git clone https://github.com/qt/qt5.git
+    cd qt5
+    perl init-repository
+    cd ..
+fi
+
+cd /qt/qt5
+git fetch
+git submodule foreach --recursive git fetch
+git checkout v$0
+git submodule foreach --recursive git reset --hard
+git submodule foreach --recursive git clean -dxf
+git submodule update --init --recursive
+
+cd /
+mkdir build
+cd build
+/qt/qt5/configure \
+    -release \
+    -nomake examples \
+    -nomake tests \
+    -skip qttasktree \
+    -zstd \
+    -webengine-proprietary-codecs \
+    -prefix /opt/qt/$0/gcc_64
+
+# WebEngine seems to fail the first time
+cmake --build . --parallel $(($(nproc)+1)) || true
+cmake --build . --parallel $(($(nproc)+1))
+
+cmake --install .
+cp config.summary /opt/qt/$0/gcc_64
+
+cd /opt/qt/$0
+tar cvfpJ /root/export/Qt-amd64-$0.tar.xz gcc_64
+' $qt_version
---
+++ 6.10.1/build_6.10.1_arm64.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#
+# Author:  Luca Carlon
+# Company: -
+# Date:    2025.11.22
+#
+
+qt_version=6.10.1
+qt_src=qt-src
+
+podman run -it --rm --name qt-builder \
+    -v $PWD/../qt_export:/root/export \
+    -v "$qt_src":/qt \
+    docker.io/carlonluca/qt-builder:noble-17-36-27.2.12479018 bash -c '
+
+set -e
+
+export qtversion=$0
+mkdir -p /opt/qt/$qtversion
+cd /opt/qt/$qtversion
+tar xvfp /root/export/Qt-amd64-$qtversion.tar.xz
+
+cd /qt
+if [ ! -d /qt/qt5 ]; then
+    git clone https://code.qt.io/qt/qt5.git
+    cd qt5
+    perl init-repository
+    cd ..
+fi
+
+cd /qt/qt5
+git fetch
+git submodule foreach --recursive git fetch
+git checkout v$qtversion
+git submodule foreach --recursive git reset --hard
+git submodule foreach --recursive git clean -dxf
+git submodule update --init --recursive
+
+cd /
+mkdir build
+cd build
+/qt/qt5/configure \
+    -zstd \
+    -release \
+    -nomake examples \
+    -nomake tests \
+    -skip qttasktree \
+    -skip qtwebengine \
+    -qt-host-path /opt/qt/$qtversion/gcc_64 \
+    -prefix /opt/qt/$qtversion/gcc_arm64 -- \
+    -DCMAKE_TOOLCHAIN_FILE=/root/toolchain.cmake \
+    -DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON \
+    -DCMAKE_BUILD_WITH_INSTALL_RPATH=/opt/qt/$qtversion/gcc_arm64
+cmake --build . --parallel $(($(nproc)+4))
+cmake --install .
+cp config.summary /opt/qt/$qtversion/gcc_64/
+cd /opt/qt/$qtversion
+tar cvfpJ /root/export/Qt-arm64-$qtversion.tar.xz gcc_arm64
+' $qt_version
